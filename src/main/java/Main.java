f45abbb4d4a1256fcddb3fe48ddff90cfdbbd379
DennisTran <tranhoangson209@gmail.com>
2025-05-03 21:52:31 +0700

    none

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -148,7 +148,6 @@ public class Main {
         private void number() {
             while (isDigit(peek())) advance();
 
-            // Look for a fractional part.
             if (peek() == '.' && isDigit(peekNext())) {
                 // Consume the "."
                 advance();
@@ -362,6 +361,147 @@ public class Main {
         }
     }
 
+    static class Interpreter implements Expr.Visitor<Object> {
+        static void runtimeError(RuntimeError error) {
+            System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+            System.exit(70);
+        }
+
+        void interpret(Expr expression) { 
+            try {
+                Object value = evaluate(expression);
+                System.out.println(stringify(value));
+            } catch (RuntimeError error) {
+                runtimeError(error);
+            }
+        }
+
+        private String stringify(Object object) {
+            if (object == null) return "nil";
+
+
+            if (object instanceof Double) {
+                String text = object.toString();
+                if (text.endsWith(".0")) {
+                    text = text.substring(0, text.length() - 2);
+                }
+                return text;
+            }
+
+            return object.toString();
+        }
+
+
+        private Object evaluate(Expr expr) {
+            return expr.accept(this);
+        }
+
+        public Object visitLiteralExpr(Expr.Literal expr) {
+            return expr.value;
+        }
+
+        public Object visitGroupingExpr(Expr.Grouping expr) {
+            return evaluate(expr.expression);
+        }
+
+        class RuntimeError extends RuntimeException {
+            final Token token;
+
+            RuntimeError(Token token, String message) {
+                super(message);
+                this.token = token;
+            }
+        }
+
+        private void checkNumberOperand(Token operator, Object operand) {
+            if (operand instanceof Double) return;
+            throw new RuntimeError(operator, "Operand must be a number.");
+        }
+
+        public Object visitUnaryExpr(Expr.Unary expr) {
+            Object right = evaluate(expr.right);
+
+            switch (expr.operator.type) {
+                case MINUS:
+                    checkNumberOperand(expr.operator, right);
+                    return -(double)right;
+
+                case BANG:
+                    return !isTruthy(right);
+            }
+
+            // Unreachable.
+            return null;
+        }
+
+        private boolean isTruthy(Object object) {
+            if (object == null) return false;
+            if (object instanceof Boolean) return (boolean)object;
+            return true;
+        }
+
+        private void checkNumberOperands(Token operator,
+                                   Object left, Object right) {
+            if (left instanceof Double && right instanceof Double) return;
+    
+            throw new RuntimeError(operator, "Operands must be numbers.");
+        }
+
+        public Object visitBinaryExpr(Expr.Binary expr) {
+            Object left = evaluate(expr.left);
+            Object right = evaluate(expr.right); 
+
+            switch (expr.operator.type) {
+                case MINUS:
+                    checkNumberOperands(expr.operator, left, right);
+                    return (double)left - (double)right;
+                case SLASH:
+                    checkNumberOperands(expr.operator, left, right);
+                    return (double)left / (double)right;
+                case STAR:
+                    checkNumberOperands(expr.operator, left, right);
+                    return (double)left * (double)right;
+                case PLUS:
+                    if (left instanceof Double && right instanceof Double) {
+                        return (double) left + (double) right;
+                    } 
+
+                    if (left instanceof String && right instanceof String) {
+                        return (String) left + (String) right;
+                    }
+
+                    throw new RuntimeError(expr.operator,
+                    "Operands must be two numbers or two strings.");
+                case GREATER:
+                    checkNumberOperands(expr.operator, left, right);
+                    return (double)left > (double)right;
+                case GREATER_EQUAL:
+                    checkNumberOperands(expr.operator, left, right);
+                    return (double)left >= (double)right;
+                case LESS:
+                    checkNumberOperands(expr.operator, left, right);
+                    return (double)left < (double)right;
+                case LESS_EQUAL:
+                    return (double)left <= (double)right;
+                case BANG_EQUAL: 
+                    return !isEqual(left, right);
+                case EQUAL_EQUAL: 
+                    return isEqual(left, right);
+            }
+
+            // Unreachable.
+            return null;
+        }
+
+        private boolean isEqual(Object a, Object b) {
+            if (a == null && b == null) return true;
+            if (a == null) return false;
+
+            return a.equals(b);
+        }
+    }
+
     static class Parser{
         private static List <Token> tokens;
         private static int current = 0;
@@ -500,7 +640,6 @@ public class Main {
             System.err.println("[line " + token.line + "] Error at " +
                 (token.type == TokenType.EOF ? "end" : "'" + token.lexeme + "'") +
                 ": " + message);
-            //return new ParseError();
             System.exit(65);
         }
 
@@ -523,6 +662,15 @@ public class Main {
         for (Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    static void evaluateLine(String source) {
+        Scanner scanner = new Scanner(source);
+        List <Token> tokens = scanner.scanTokens();
+
+        Parser pa = new Parser(tokens);
+        Interpreter interpreter = new Interpreter();
+        interpreter.interpret(pa.expression());
     }
 
     public static void main(String[] args) {
@@ -551,6 +699,9 @@ public class Main {
             System.exit(errors);
         } else if (command.equals("parse")) {
             parseLine(fileContents);
+            System.exit(errors);
+        } else if (command.equals("evaluate")) {
+            evaluateLine(fileContents);
             System.exit(errors);
         }
     }
